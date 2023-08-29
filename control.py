679edad89f9a9298b200509b542fb525fe93b609
Noah S. Roberts <noah10838@gmail.com>
2023-08-28 22:08:44 -0600

    okay control.py worky, me bed now

--- control.py
+++ control.py
@@ -1,19 +1,21 @@
 import sys, does_shit, argparse
 
-s_in = sys.stdin.read(6)
-s_out = sys.stdout
 s_err = sys.stderr
+
 
 parser = argparse.ArgumentParser(prog="Keyboard Brightness Controller", description="Increase, decrease, or set the keyboard brightness level in Linux + GNOME\nThis functionality is missing from GNOME Desktop, so I made it myself.", epilog="Keyboard Brightness Controller is subject to copyright under the GNU General Public License (v3). See LICENSE for details.")
 
-parser.add_argument("mode", choices=["inc", "dec", "set"], default="set", dest="mode")
+parser.add_argument('mode', choices=['inc', 'dec', 'set'], default='set', help='Selects the mode of operation: inc to increase by the specified amount; dec to decrease by the specified amount; set to snap to the specified amount.')
+parser.add_argument("percent", choices=range(1,100), type=int, help="percentage to increase/decrease by or to set to", default=10)
+
+args = parser.parse_args()
 
 
 max = does_shit.max_brightness()
-value = int(s_in[1] / 100 * max)
+value = int(args.percent / 100 * max)
 
 
-if parser.mode == "set":
+if args.mode == "set":
     output = does_shit.set_brightness(value)
     if output < 0:
         s_err.write("ERROR: failed to write brightness value to file")
@@ -22,7 +24,7 @@ if parser.mode == "set":
         print(output)
 
 
-if parser.mode == "inc":
+if args.mode == "inc":
     current = does_shit.current_brightness()
     if current + value <= max:
         does_shit.set_brightness(value)
@@ -30,7 +32,7 @@ if parser.mode == "inc":
         does_shit.set_brightness(255)
 
 
-if parser.mode == "dec":
+if args.mode == "dec":
     current = does_shit.current_brightness()
     if current - value >= 0:
         does_shit.set_brightness(value)
