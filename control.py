63d6eb77502499b1a6429c0a784f2d079fab20de
Noah S. Roberts <noah10838@gmail.com>
2023-08-28 21:54:40 -0600

    trying argparse now

--- control.py
+++ control.py
@@ -1,39 +1,19 @@
-import sys, does_shit
+import sys, does_shit, argparse
 
-s_in = sys.stdin
+s_in = sys.stdin.read(6)
 s_out = sys.stdout
 s_err = sys.stderr
 
-modes = {
-    "inc": 0,
-    "dec": 1,
-    "set": 2,
-    "help": 3,
-    "?": 3
-}
-mode = modes[s_in[0]]
+parser = argparse.ArgumentParser(prog="Keyboard Brightness Controller", description="Increase, decrease, or set the keyboard brightness level in Linux + GNOME\nThis functionality is missing from GNOME Desktop, so I made it myself.", epilog="Keyboard Brightness Controller is subject to copyright under the GNU General Public License (v3). See LICENSE for details.")
+
+parser.add_argument("mode", choices=["inc", "dec", "set"], default="set", dest="mode")
+
+
 max = does_shit.max_brightness()
 value = int(s_in[1] / 100 * max)
 
 
-if mode == 3:
-    print("""
-          Keyboard Brightness Controller
-          by Noah S. Roberts, copyright via GPLv3
-
-          --- SYNTAX ---
-          control.py inc|dec|set|help <value>
-
-              inc - increase brightness by <value> percent
-              dec - decrease brightness by <value> percent
-              set - set brightness to <value> percent
-              help|? - display this help output
-          
-          Keyboard Brightness Controller is subject to copyright under the GNU General Public License (v3). See LICENSE for details.
-    """)
-
-
-if mode == 2:
+if parser.mode == "set":
     output = does_shit.set_brightness(value)
     if output < 0:
         s_err.write("ERROR: failed to write brightness value to file")
@@ -42,7 +22,7 @@ if mode == 2:
         print(output)
 
 
-if mode == 1:
+if parser.mode == "inc":
     current = does_shit.current_brightness()
     if current + value <= max:
         does_shit.set_brightness(value)
@@ -50,7 +30,7 @@ if mode == 1:
         does_shit.set_brightness(255)
 
 
-if mode == 0:
+if parser.mode == "dec":
     current = does_shit.current_brightness()
     if current - value >= 0:
         does_shit.set_brightness(value)
